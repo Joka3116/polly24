256c2dcbc1b8013a74b33f1eb85d7e7e00b6477a
Petter Lidberg <petter@lidberg.se>
2025-12-15 00:11:17 +0100

    Added About view

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import StartView from '../views/StartView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import StartView from "../views/StartView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Start',
-      component: StartView
-    },
-      {
-      path: '/poll/:id',
-      name: 'PollView',
-      component: () => import('../views/PollView.vue')
+      path: "/",
+      name: "Start",
+      component: StartView,
     },
     {
-      path: '/lobby/:id',
-      name: 'LobbyView',
-      component: () => import('../views/LobbyView.vue')
+      path: "/poll/:id",
+      name: "PollView",
+      component: () => import("../views/PollView.vue"),
     },
     {
-      path: '/create/',
-      name: 'CreateView',
-      component: () => import('../views/CreateView.vue')
+      path: "/lobby/:id",
+      name: "LobbyView",
+      component: () => import("../views/LobbyView.vue"),
     },
     {
-      path: '/result/:id',
-      name: 'ResultView',
-      component: () => import('../views/ResultView.vue')
-    }
-  ]
-})
+      path: "/create/",
+      name: "CreateView",
+      component: () => import("../views/CreateView.vue"),
+    },
+    {
+      path: "/result/:id",
+      name: "ResultView",
+      component: () => import("../views/ResultView.vue"),
+    },
+    {
+      path: "/about/",
+      name: "AboutView",
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/AboutView.vue
@@ -0,0 +1,99 @@
+<template>
+    <header>
+        <img src="/img/logo.png" alt="Logo" />
+        <h1>{{ uiLabels.about }}</h1>
+    </header>
+    <main></main>
+    <FooterComponent></FooterComponent>
+    <ResponsiveNav>
+        <router-link to="/about/">
+            {{ uiLabels.about || "ABOUT" }}
+        </router-link>
+        <router-link to="/lobby/1">
+            {{ uiLabels.play || "PLAY" }}
+        </router-link>
+        <router-link to="/create/">
+            {{ uiLabels["createGame"] || "CREATE" }}
+        </router-link>
+        <button v-on:click="switchLanguage">
+            {{ uiLabels.changeLanguage }}
+        </button>
+    </ResponsiveNav>
+</template>
+
+<script>
+import ResponsiveNav from "@/components/ResponsiveNav.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import io from "socket.io-client";
+const socket = io("localhost:3000");
+
+export default {
+    name: "AboutView",
+    components: {
+        ResponsiveNav,
+        FooterComponent,
+    },
+    data: function () {
+        return {
+            uiLabels: {},
+            newPollId: "",
+            lang: localStorage.getItem("lang") || "en",
+            hideNav: true,
+            showPollInput: false,
+        };
+    },
+    created: function () {
+        socket.on("uiLabels", (labels) => (this.uiLabels = labels));
+        socket.emit("getUILabels", this.lang);
+    },
+    methods: {
+        switchLanguage: function () {
+            if (this.lang === "en") {
+                this.lang = "sv";
+            } else {
+                this.lang = "en";
+            }
+            localStorage.setItem("lang", this.lang);
+            socket.emit("getUILabels", this.lang);
+        },
+        toggleNav: function () {
+            this.hideNav = !this.hideNav;
+        },
+    },
+};
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 4rem;
+}
+
+header img {
+    width: 70%;
+    height: auto;
+    max-width: 30rem;
+    max-height: 30rem;
+    aspect-ratio: 1;
+    padding-bottom: 1rem;
+}
+
+header h1,
+h2 {
+    font-size: 2rem;
+    color: white;
+    text-align: center;
+}
+
+main {
+    color: white;
+    padding-top: 5rem;
+    padding-bottom: 10rem;
+}
+
+main a {
+    color: white;
+}
+</style>
