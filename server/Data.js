f2c65441ce6864d3e9bc531a924811923d13c890
eddkra <eddie.krantz.8350@student.uu.se>
2025-12-15 11:48:19 +0100

    Update Data.js

--- server/Data.js
+++ server/Data.js
@@ -5,7 +5,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "pswd",
+  password: "PaSswOrD",
   database: "quizgame"
 });
 
