42a2049b7a08daa35b4e5dfe9bd0d1abc4bfda66
Petter Lidberg <petter@lidberg.se>
2026-01-06 21:55:19 +0100

    Min padding for all views

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,15 @@ import AppFooter from "@/components/AppFooter.vue";
     flex-direction: column;
     min-height: 100vh !important;
     background: radial-gradient(circle, #0f2845, #02050a);
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+
+@media (min-width: 1024px) {
+    .app-container {
+        padding-left: 1.25rem;
+        padding-right: 1.25rem;
+    }
 }
 
 :deep(> div:first-child) {
